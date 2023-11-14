6aececbf11a7bd9febe02d54014b8b578b6c90e7
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-14 22:35:52 +0100

    Creating cucumber runner class

---
+++ src/test/java/cucumberOptions/FailedTestRunner.java
@@ -0,0 +1,22 @@
+package cucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(features = "@target/failed_scenarios.txt", glue = "stepDefinitions",
+        monochrome = true,
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
+
+
+
+}
---
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -0,0 +1,23 @@
+package cucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(features = "src/test/resources", glue = "stepDefinitions", monochrome = true,
+        tags = "@PlaceOrder or @OffersPage",
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "rerun:target/failed_scenarios.txt"})
+public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
+
+
+
+}
